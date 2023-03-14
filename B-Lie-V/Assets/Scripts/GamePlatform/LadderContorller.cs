99df3b2ac4298ef6aac7699a79280c6502cb3f25
M.R <m.r.MeijoUniv@gmail.com>
2023-03-15 02:45:30 +0900

    [update]梯子システムを一旦削除、[add]全体を見れる地図を追加

--- B-Lie-V/Assets/Scripts/GamePlatform/LadderContorller.cs
+++ B-Lie-V/Assets/Scripts/GamePlatform/LadderContorller.cs
@@ -12,15 +12,11 @@ public class LadderContorller : MonoBehaviour
     [SerializeField] private GameObject relayPoint;
 
     private Rigidbody playerRigidBody;
-    private ContinuousMoveProviderBase moveProvider;
-    private ContinuousTurnProviderBase turnProvider;
-    private bool isRelayPoint = false;
+    private float longPressTime = 0;
     
     private void Start() 
     {
         playerRigidBody = player.GetComponent<Rigidbody>();
-        moveProvider = GetComponentInChildren<ContinuousMoveProviderBase>();
-        turnProvider  = GetComponentInChildren<ContinuousTurnProviderBase>();
     }
 
     private void OnTriggerEnter(Collider other) 
@@ -28,7 +24,19 @@ public class LadderContorller : MonoBehaviour
         if(other.gameObject.CompareTag("Player"))
         {
             currentCanvas.SetActive(true);
-            StartCoroutine(LadderAction());
+        }
+    }
+    
+    private void OnTriggerStay(Collider other) 
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickUp, OVRInput.Controller.RTouch))
+        {
+            player.transform.position = aboveTorus.transform.position;
+        }
+
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickDown, OVRInput.Controller.RTouch))
+        {
+            player.transform.position = underTorus.transform.position;
         }
     }
 
@@ -37,44 +45,6 @@ public class LadderContorller : MonoBehaviour
         if(other.gameObject.CompareTag("Player"))
         {
             currentCanvas.SetActive(false);
-            StopCoroutine(LadderAction());
-        }
-    }
-
-    IEnumerator LadderAction()
-    {
-        while(true)
-        {
-            if(OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
-            {
-                playerRigidBody.constraints = RigidbodyConstraints.FreezePosition;
-                moveProvider.enabled = false;
-                turnProvider.enabled = false;
-                player.transform.position = relayPoint.transform.position;
-                isRelayPoint = true;
-            }
-
-            if(OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickUp, OVRInput.Controller.RTouch) && isRelayPoint)
-            {
-                isRelayPoint = false;
-                player.transform.position = aboveTorus.transform.position;
-                playerRigidBody.constraints = RigidbodyConstraints.None;
-                moveProvider.enabled = true;
-                turnProvider.enabled = true;
-                yield break;
-            }
-
-            if(OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickDown, OVRInput.Controller.LTouch) && isRelayPoint)
-            {
-                isRelayPoint = false;
-                player.transform.position = underTorus.transform.position;
-                playerRigidBody.constraints = RigidbodyConstraints.None;
-                moveProvider.enabled = true;
-                turnProvider.enabled = true;
-                yield break;
-            }
-
-            yield return new WaitForSeconds(0.01f);
         }
     }
 }
